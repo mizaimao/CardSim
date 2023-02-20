4351d2d2f7c648eead960fd59265befe3daa222d
mizaimao <frankzhang930@gmail.com>
2023-02-20 18:21:46 -0500

    reverted back to opencv

--- cards/game.py
+++ cards/game.py
@@ -1,85 +1,34 @@
 """Main game entry-point."""
-
-from ursina import (
-    camera,
-    color,
-    destroy,
-    load_model,
-    load_texture,
-    Entity,
-    Ursina,
-    Mesh,
-    Vec3,
-)
-from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+import numpy as np
+import cv2
 
 
-app = Ursina()
+FRAME_WIDTH: int = 1280
+FRAME_HEIGHT: int = 800
+WINDOW_NAME: str = "Chicken"
 
-player = PlatformerController2d(y=1, z=0.01, scale_y=1, max_jumps=2)
-ground = Entity(model="quad", scale_x=10, collider="box", color=color.black)
-quad = load_model("quad", use_deepcopy=True)
+test_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 
-level_parent = Entity(model=Mesh(vertices=[], uvs=[]), texture="white_cube")
+def game():
+    """Entry-point."""
+    key: int = ord('n')
+    color_index: int = 0
+    while key == ord('n'):
+        
+        if color_index == len(test_colors):
+            color_index = 0
+        image: np.ndarray = np.full(
+            (FRAME_HEIGHT, FRAME_WIDTH, 3), test_colors[color_index], dtype=np.uint8
+        )
+        cv2.imshow(WINDOW_NAME, image)
 
+        key = cv2.waitKey(0)
+        color_index += 1
 
-def make_level(texture):
-    # destroy every child of the level parent.
-    # This doesn't do anything the first time the level is generated, but if we want to update it several times
-    # this will ensure it doesn't just create a bunch of overlapping entities.
-    [destroy(c) for c in level_parent.children]
-
-    for y in range(texture.height):
-        collider = None
-        for x in range(texture.width):
-            col = texture.get_pixel(x, y)
-
-            # If it's black, it's solid, so we'll place a tile there.
-            if col == color.black:
-                level_parent.model.vertices += [
-                    Vec3(*e) + Vec3(x + 0.5, y + 0.5, 0)
-                    for e in quad.generated_vertices
-                ]  # copy the quad model, but offset it with Vec3(x+.5,y+.5,0)
-                level_parent.model.uvs += quad.uvs
-                # Entity(parent=level_parent, position=(x,y), model='cube', origin=(-.5,-.5), color=color.gray, texture='white_cube', visible=True)
-                if not collider:
-                    collider = Entity(
-                        parent=level_parent,
-                        position=(x, y),
-                        model="quad",
-                        origin=(-0.5, -0.5),
-                        collider="box",
-                        visible=False,
-                    )
-                else:
-                    # instead of creating a new collider per tile, stretch the previous collider right.
-                    collider.scale_x += 1
-            else:
-                collider = None
-
-            # If it's green, we'll place the player there. Store this in player.start_position so we can reset the plater position later.
-            if col == color.green:
-                player.start_position = (x, y)
-                player.position = player.start_position
-
-    level_parent.model.generate()
+        
+    cv2.destroyAllWindows()
 
 
-make_level(load_texture("platformer_tutorial_level"))  # generate the level
-
-camera.orthographic = True
-camera.position = (30 / 2, 8)
-camera.fov = 16
-
-
-player.traverse_target = level_parent
-enemy = Entity(model="cube", collider="box", color=color.red, position=(16, 5, -0.1))
-
-
-def update():
-    if player.intersects(enemy).hit:
-        print("die")
-        player.position = player.start_position
-
-
-app.run()
+if __name__ == "__main__":
+    """Main function."""
+    game()
