d8984c5de318666015d91cd1827aa5bb51b272f4
mizaimao <frankzhang930@gmail.com>
2023-02-20 18:07:53 -0500

    linting

--- cards/game.py
+++ cards/game.py
@@ -1,17 +1,27 @@
 """Main game entry-point."""
 
-from ursina import camera, color, destroy, load_model, load_texture, Entity, Ursina, Mesh, Vec3
+from ursina import (
+    camera,
+    color,
+    destroy,
+    load_model,
+    load_texture,
+    Entity,
+    Ursina,
+    Mesh,
+    Vec3,
+)
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
-
 
 
 app = Ursina()
 
-player = PlatformerController2d(y=1, z=.01, scale_y=1, max_jumps=2)
-ground = Entity(model='quad', scale_x=10, collider='box', color=color.black)
-quad = load_model('quad', use_deepcopy=True)
+player = PlatformerController2d(y=1, z=0.01, scale_y=1, max_jumps=2)
+ground = Entity(model="quad", scale_x=10, collider="box", color=color.black)
+quad = load_model("quad", use_deepcopy=True)
 
-level_parent = Entity(model=Mesh(vertices=[], uvs=[]), texture='white_cube')
+level_parent = Entity(model=Mesh(vertices=[], uvs=[]), texture="white_cube")
+
 
 def make_level(texture):
     # destroy every child of the level parent.
@@ -22,15 +32,25 @@ def make_level(texture):
     for y in range(texture.height):
         collider = None
         for x in range(texture.width):
-            col = texture.get_pixel(x,y)
+            col = texture.get_pixel(x, y)
 
             # If it's black, it's solid, so we'll place a tile there.
             if col == color.black:
-                level_parent.model.vertices += [Vec3(*e) + Vec3(x+.5,y+.5,0) for e in quad.generated_vertices] # copy the quad model, but offset it with Vec3(x+.5,y+.5,0)
+                level_parent.model.vertices += [
+                    Vec3(*e) + Vec3(x + 0.5, y + 0.5, 0)
+                    for e in quad.generated_vertices
+                ]  # copy the quad model, but offset it with Vec3(x+.5,y+.5,0)
                 level_parent.model.uvs += quad.uvs
                 # Entity(parent=level_parent, position=(x,y), model='cube', origin=(-.5,-.5), color=color.gray, texture='white_cube', visible=True)
                 if not collider:
-                    collider = Entity(parent=level_parent, position=(x,y), model='quad', origin=(-.5,-.5), collider='box', visible=False)
+                    collider = Entity(
+                        parent=level_parent,
+                        position=(x, y),
+                        model="quad",
+                        origin=(-0.5, -0.5),
+                        collider="box",
+                        visible=False,
+                    )
                 else:
                     # instead of creating a new collider per tile, stretch the previous collider right.
                     collider.scale_x += 1
@@ -44,19 +64,21 @@ def make_level(texture):
 
     level_parent.model.generate()
 
-make_level(load_texture('platformer_tutorial_level'))   # generate the level
+
+make_level(load_texture("platformer_tutorial_level"))  # generate the level
 
 camera.orthographic = True
-camera.position = (30/2,8)
+camera.position = (30 / 2, 8)
 camera.fov = 16
 
 
-
 player.traverse_target = level_parent
-enemy = Entity(model='cube', collider='box', color=color.red, position=(16,5,-.1))
+enemy = Entity(model="cube", collider="box", color=color.red, position=(16, 5, -0.1))
+
+
 def update():
     if player.intersects(enemy).hit:
-        print('die')
+        print("die")
         player.position = player.start_position
 
 
