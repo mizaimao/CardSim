25b560757945227f032399fd542ec661879a7a22
mizaimao <frankzhang930@gmail.com>
2023-02-20 19:01:10 -0500

    table object skeleton

--- cards/game.py
+++ cards/game.py
@@ -2,30 +2,31 @@
 import numpy as np
 import cv2
 
+from cards.objects.card_table import CardTable
+
 
 FRAME_WIDTH: int = 1280
 FRAME_HEIGHT: int = 800
 WINDOW_NAME: str = "Chicken"
 
-test_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
 
 def game():
     """Entry-point."""
+
+    card_table: CardTable = CardTable(
+        frame_width=FRAME_WIDTH, frame_height=FRAME_HEIGHT, color_format="BGR"
+    )
+
     key: int = ord('n')
-    color_index: int = 0
+    
     while key == ord('n'):
         
-        if color_index == len(test_colors):
-            color_index = 0
-        image: np.ndarray = np.full(
-            (FRAME_HEIGHT, FRAME_WIDTH, 3), test_colors[color_index], dtype=np.uint8
-        )
+        image: np.ndarray = card_table.iter_image()
         cv2.imshow(WINDOW_NAME, image)
 
         key = cv2.waitKey(0)
-        color_index += 1
-
-        
+                
     cv2.destroyAllWindows()
 
 
