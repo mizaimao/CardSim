88e7bcef7a41a8f420a322d12a13bdc54895b677
mizaimao <frankzhang930@gmail.com>
2023-02-21 18:05:28 -0500

    refactored card table visual class

--- cards/game.py
+++ cards/game.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 
-from cards.objects.card_table import CardTable
+from cards.visual.visual_manger import VisualManager
 
 
 FRAME_WIDTH: int = 1280
@@ -14,15 +14,15 @@ WINDOW_NAME: str = "Chicken"
 def game():
     """Entry-point."""
 
-    card_table: CardTable = CardTable(
+    vman: VisualManager = VisualManager(
         frame_width=FRAME_WIDTH, frame_height=FRAME_HEIGHT, color_format="BGR"
     )
+
 
     key: int = ord('n')
     
     while key == ord('n'):
-        
-        image: np.ndarray = card_table.iter_image()
+        image: np.ndarray = vman.get_table_image()
         cv2.imshow(WINDOW_NAME, image)
 
         key = cv2.waitKey(0)
